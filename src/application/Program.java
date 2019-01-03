049374c13112ad1a48cab3165653d1c1ab6f38d2
RodrigoYukata <rodrigoyukata@gmail.com>
2019-01-03 16:46:29 -0200

    moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,30 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		Scanner in = new Scanner(System.in);
+		
+		while(true) {
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(in);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(in);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+		
+		
+	
 		
 	}
 
